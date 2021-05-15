63a65adbbd28b1c33fd74a66f690f793d1de87b0
Søren Mehlsen <202000071@post.au.dk>
2021-05-15 19:05:49 +0200

    Opdatering

--- DataLayer/DataBase.cs
+++ DataLayer/DataBase.cs
@@ -13,32 +13,81 @@ namespace DataLayer
         private SqlConnection connection;
         private SqlDataReader reader;
         private SqlCommand command;
-        private String const DBlogin;
-        
+        private const String DBlogin = "F21ST2ITS2au639422";
+
+
         public DataBase() { }
 
         public bool isUserRegistered(String socSecNb, String pw)
         {
-            return true; 
+            connection = new SqlConnection("Data Source=st-i4dab.uni.au.dk;Initial Catalog=" + DBlogin + ";User ID=" + DBlogin + ";Password=" + DBlogin + ";Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            bool result = false;
+
+            command = new SqlCommand("select * from RegisteredUsers where SocSecNb ='" + socSecNb + "' AND PW = " + pw + "", connection);
+
+            connection.Open();
+
+            reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                if (Convert.ToBoolean(reader["SocSecNb"]) == true)
+                    result = true; 
+                else
+                    result = false; 
+            }
+            connection.Close();
+            return result; 
         }
 
         public int getHeight(String socSecNb)
         {
-            return 1;
+            connection = new SqlConnection("Data Source=st-i4dab.uni.au.dk;Initial Catalog=" + DBlogin + ";User ID=" + DBlogin + ";Password=" + DBlogin + ";Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            int result = 0;
+
+            command = new SqlCommand("select * from RegisteredUsers where SocSecNb ='" + socSecNb + "'", connection);
+
+            connection.Open();
+
+            reader = command.ExecuteReader();
+
+            result = Convert.ToInt32(reader["Height"]);
+
+            connection.Close();
+            return result;
         }
 
         public List<DTO_Weight> getWeightData(String socSecNb)
         {
-            return;
+            connection = new SqlConnection("Data Source=st-i4dab.uni.au.dk;Initial Catalog=" + DBlogin + ";User ID=" + DBlogin + ";Password=" + DBlogin + ";Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            List<DTO_Weight> weightList = new List<DTO_Weight>();
+
+            command = new SqlCommand("select * from WeightData where SocSecNb ='" + socSecNb + "'", connection);
+
+            connection.Open();
+
+            reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                weightList.Add(new DTO_Weight(Convert.ToDouble(reader["Weight"]), 0, Convert.ToDateTime(reader["Date"])));
+            }
+            connection.Close();
+            return weightList;
         }
 
         public List<DTO_BSugar> getBSugarData(String socSecNb)
         {
+            connection = new SqlConnection("Data Source=st-i4dab.uni.au.dk;Initial Catalog=" + DBlogin + ";User ID=" + DBlogin + ";Password=" + DBlogin + ";Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             return;
         }
 
         public List<DTO_BPressure> getBPressureData(String socSecNb)
         {
+            connection = new SqlConnection("Data Source=st-i4dab.uni.au.dk;Initial Catalog=" + DBlogin + ";User ID=" + DBlogin + ";Password=" + DBlogin + ";Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             return;
         }
     }
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -13,4 +13,47 @@
 --Date date)
 
 --insert into WeightData (SocSecNb, Weight, Date) values ('123456-7890',86.4,'2015-01-03')
-insert into WeightData (SocSecNb, Weight, Date) values ('123456-7890',86.4,'2015-01-03')
+--insert into WeightData (SocSecNb, Weight, Date) values ('234567-8901',75.8,'2015-01-03')
+--insert into WeightData (SocSecNb, Weight, Date) values ('345678-9012',75.8,'2015-01-03')
+--insert into WeightData (SocSecNb, Weight, Date) values ('123456-7890',95.1,'2015-01-03')
+--insert into WeightData (SocSecNb, Weight, Date) values ('123456-7890',85.3,'2015-01-07')
+--insert into WeightData (SocSecNb, Weight, Date) values ('234567-8901',81.1,'2015-01-10')
+--insert into WeightData (SocSecNb, Weight, Date) values ('345678-9012',72.0,'2015-01-06')
+--insert into WeightData (SocSecNb, Weight, Date) values ('234567-8901',95.3,'2015-01-07')
+--insert into WeightData (SocSecNb, Weight, Date) values ('345678-9012',86.4,'2015-01-12')
+--insert into WeightData (SocSecNb, Weight, Date) values ('123456-7890',82.3,'2015-01-11')
+
+--create table BloodSugarData (
+--SocSecNb nvarchar(11),
+--BloodSugar decimal(3,2),
+--Date date)
+
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('123456-7890',8.0,'2015-01-05')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('234567-8901',5.3,'2015-01-05')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('345678-9012',8.7,'2015-01-05')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('123456-7890',8.6,'2015-01-06')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('123456-7890',4.1,'2015-01-09')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('234567-8901',6.5,'2015-01-06')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('345678-9012',7.7,'2015-01-07')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('234567-8901',9.8,'2015-01-08')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('345678-9012',9.2,'2015-01-11')
+--insert into BloodSugarData (SocSecNb, BloodSugar, Date) values ('123456-7890',5.4,'2015-01-12')
+
+--create table BloodPressure (
+--SocSecNb nvarchar(11),
+--Systolic int,
+--Diastolic int,
+--Date date)
+
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('123456-7890',110,80,'2015-01-5')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('234567-8901',115,85,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('345678-9012',120,75,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('123456-7890',125,100,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('123456-7890',130,95,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('234567-8901',135,90,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('345678-9012',140,85,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('234567-8901',110,80,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('345678-9012',115,100,'2015-01-05')
+--insert into BloodPressure (SocSecNb, Systolic, Diastolic, Date) values ('123456-7890',120,95,'2015-01-05')
+
+select * from RegisteredUsers where SocSecNb ='123456-7890' AND PW = 1234  
